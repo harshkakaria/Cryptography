087a8aaeb21e4917591dab8addcbaa96b3b44c7d
harshkakaria <harshkakaria1@gmail.com>
2021-01-19 19:45:29 +0530

    Second Commit

--- Crypto.py
+++ Crypto.py
@@ -1,11 +1,25 @@
+from string import *
+message = input("enter your message: ")
+import time
 def main():
     letters = 'abcdefhgijklmnopqrstuvxyz'
-    val = keys[-1] + keys[0:-1]
+    val = letters[-1] + letters[0:-1]
 
     encrypt = dict(zip(letters,val))
     Decrypt = dict(zip(letters,val))
 
-    message = input("enter your message: ")
     e_D = input("E / D: ")
+
     if e_D == "E":
-        
+        mssg = ''.join([encrypt[letter]
+                            for letter in message()])
+    elif e_D == "D":
+        mssg = ''.join([Decrypt[letter]
+                            for letter in message()])
+
+    else:
+        print("invalid Command")
+
+    return mssg
+if __name__ == '__main__':
+    print(main())
